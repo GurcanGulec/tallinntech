1e993acd02a549eda291a3ca63ed83abc75bec0b
Gürcan Güleç <gugule@ttu.ee>
2016-10-16 21:50:26 +0300

    Updating homework1.c

--- homework1/homework1.c
+++ homework1/homework1.c
@@ -1,9 +1,19 @@
+/**
+ * File: homework1.c
+ * Author: Gurcan Gulec
+ * Created: 14 October 2016
+ * Last edit: 16 October 2016
+ * 
+ * Description: Function homework.
+ * 
+ */
+
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
-    float a, b, c, h, n, sum, between, value, result;
+    float a, b, c, h, n, sum, result;
 
     // ask for N and if N is less than "0" and greater than "15", ask again.
     printf("Please enter N (Between 0-15) : ");
@@ -46,41 +56,40 @@ int main()
     }
 
     // calculate H+CH in a loop and increase the power of C
-    for(int a = 0; a < n; a++)
-    {
-        between = pow(c,value)*h;
-        value++;
-    }
-
-    // calculate the function
-    result = (1 / sqrt(pow((a*between),2) - 1/a)) - (2 / (3*sqrt(5-pow((a*between),2))));
     
-    if (result != result)
+    for(int i = 0; i < n; i++)
     {
-        printf("Complex\n");
-    }
-    else if(result == INFINITY)
-    {
-        printf("Infinite\n");
-    }
-    else
-    {
-        printf("Result is: %f\n", result);
-    }
+        // calculate A + H + CH...
+        float toCalculate = a;
+        
+        for(int j = 0; j < i; j++)
+        {
+            toCalculate += pow(c,j)*h;
+        }
+        
 
-    // check the result. if it is infinite print out "Infinite". if it is a complex number, print out "Complex"
-    //if (result != result)
-    //{
-    //    printf("Complex");
-    //}
-    //else if(result == INFINITY)
-    //{
-    //    printf("Infinity");
-    //}
-    //else
-    //{
-    //    printf("Result is: %f", result);
-    //}
+        // calculate the function
+        result = (1 / sqrt(pow((a*toCalculate),2) - 1/a)) - (2 / (3*sqrt(5-pow((a*toCalculate),2))));
+        // check if it is a complex number
+        if (result != result)
+        {
+            printf("%.2f \t", toCalculate);
+            printf("Complex\n");
+        }
+        // check if it is infinite
+        else if(result == INFINITY)
+        {
+            printf("%.2f \t", toCalculate);
+            printf("Infinite\n");
+        }
+        // show the number
+        else
+        {
+            printf("%.2f \t", toCalculate);
+            printf("%f\n", result);
+        }
+        
+    }
 
     return 0;
     
